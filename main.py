b19950a896ddffad2819c8e3a52f372bf3c63dda
XnetwolfX <90906034+XnetwolfX@users.noreply.github.com>
2021-09-22 20:32:14 +0100

    From my testing lab
    A more complex version 
    And yes working

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import base64
-#not yet ready
-print("the string input requirement must be left blank only if you want to decode a file")
-string = input("string: ")
-if string =
-path = input("file path: ")
-def encode():
-strtoen = string.encode("ascii")
-
-enco = base64.b64encode(strtoen)
-
-print(enco)
-
-enco = base64.b64decode(enco)
-
-print(enco)
+import os
+def encodeit():
+	string_path = input("string to encode or file path: ")
+	print("if you choose Y that mean the program is going to treat it as a string, N mean like a path to a file or a file")
+	path_or_string = input("string Y/N: ")
+	if path_or_string == "Y" or "y":
+		
+		bytes = string_path.encode("ascii")
+		print(bytes)
+		enc = base64.b64encode(bytes)
+		print(enc)
+		
+	elif path_or_string == "N" or "n":
+		print("on testing Beta wait for upgrade")
+		os.system("exit()")
+		
+option_1 = input("Do you want to encode or decode Y for encode and N for decode Y/N: ")
+if option_1 == "Y" or "y":
+	os.system("clear")
+	print("encoding lab")
+	encodeit()
+elif option_1 == "N" or "n":
+	os.system("clear")
+	print("decoding lab")
+	
