a8fffcce2f918b1a30c08f46544bd2e1ad516a77
XnetwolfX <90906034+XnetwolfX@users.noreply.github.com>
2021-10-03 12:06:07 +0100

    From my lab
    Encode files

--- main.py
+++ main.py
@@ -1,26 +1,75 @@
 import base64
+import time
 import os
+import sys
+def design():
+	print("""
+	made by: XnetwolfX(github)
+	author : RadiationBolt
+	facebook: rad.taren
+	follow me on github for more tools
+	""")
 def encodeit():
-	string_path = input("string to encode or file path: ")
-	print("if you choose Y that mean the program is going to treat it as a string, N mean like a path to a file or a file")
-	path_or_string = input("string Y/N: ")
-	if path_or_string == "Y" or "y":
-		
-		bytes = string_path.encode("ascii")
-		print(bytes)
+	string_path = input("""
+{1} string 
+{2} file
+> """)
+	if string_path == "1":
+		string = input("give the string > ")
+		bytes = string.encode()
+		print(f"byteslike object: {bytes}")
 		enc = base64.b64encode(bytes)
 		print(enc)
 		
-	elif path_or_string == "N" or "n":
-		print("on testing Beta wait for upgrade")
-		os.system("exit()")
-		
-option_1 = input("Do you want to encode or decode Y for encode and N for decode Y/N: ")
-if option_1 == "Y" or "y":
+	elif string_path == "2":
+		path = input("give the path > ")
+		outpath = input("output file> ")
+		if outpath == "":
+			iop = input("do you want it to print in screen y/n:" )
+			if iop == "n":
+				print("you must choose an output file, restart the program")
+				sys.exit()
+			else:
+				sys.exit()
+		if path == "":
+			iop = input("do you want to encode it to a file y/n:")
+			if iop == "n":
+				print("you must have a file to encode, restart the program")
+				sys.exit()
+			else:
+				 sys.exit()
+	print("reading file")
+	f = open(path, "r")
+	
+	ren = f.read()
+	bytes = ren.encode()
+	enc = base64.b64encode(bytes)
+	print("openning/creating output file")
+	out = open(outpath, "wb")
+	print(f"{design}\nwriting encoded type of {path} to {out}")
+	out.write(enc)
+	print("writing complete")
+	out.close
+			
+start = design()		
+option_1 = input(f"""
+{start}\n
+{1} encode (base64)
+{2} decode (base64)
+
+> """)
+if option_1 == "1":
 	os.system("clear")
-	print("encoding lab")
+	print("starting encoding lab ")
+	time.sleep(2)
+	os.system("clear")
 	encodeit()
-elif option_1 == "N" or "n":
+elif option_1 == "2":
 	os.system("clear")
-	print("decoding lab")
+	print("starting decoding lab ")
+	time.sleep(2)
+	os.system("clear")
+	decodeit()
+else:
+	print("please restart the script and select an option which is listed")
 	
